db04f63604fcaa0270f6c9f1ec8e66e329daf336
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-03-14 13:02:27 +1100

    ENH: fixed filter-alignments command

--- src/mdeq_analysis/__init__.py
+++ src/mdeq_analysis/__init__.py
@@ -1,7 +1,10 @@
 """scripts for analysis steps associated with the mdeq app"""
+import pathlib
 
 import click
+
 from scitrack import CachingLogger
+
 
 __author__ = "Gavin Huttley"
 __credits__ = ["Gavin Huttley"]
@@ -14,6 +17,11 @@ def main():
     """cli for analyses assocuated with mdeq"""
 
 
+_verbose = click.option("-v", "--verbose", count=True)
+_limit = click.option("-L", "--limit", type=int, default=None)
+_overwrite = click.option("-O", "--overwrite", is_flag=True)
+
+
 @main.command()
 @click.option(
     "-d",
@@ -23,24 +31,30 @@ def main():
 )
 @click.option("-s", "--suffix", help="suffix of files within indir to be loaded")
 @click.option("-o", "--outpath", type=click.Path(), help="path to write tinydb output")
-def filter_alignments(indir, outpath, suffix):
+@_limit
+@_overwrite
+def filter_alignments(indir, outpath, suffix, limit, overwrite):
     """filters sequence alignments"""
     from cogent3.app import io, sample
 
     LOGGER = CachingLogger(create_dir=True)
     LOGGER.log_args()
-    LOGGER.log_file_path = outpath.parent / "mdeq_analysis-filter.log"
 
-    path = "../data/raw/microbial/nexus"
-    dstore = io.get_data_store(path, suffix=suffix)
+    indir = pathlib.Path(indir)
+    outpath = pathlib.Path(outpath)
+
+    LOGGER.log_file_path = outpath.parent / "mdeqasis-filter_alignments.log"
+
+    dstore = io.get_data_store(indir, suffix=suffix, limit=limit)
 
     loader = io.load_aligned(moltype="dna", format="nexus")
 
     just_nucs = sample.omit_degenerates(
         moltype="dna", motif_length=1, gap_is_degen=True
     )
-    outpath = "../data/raw/microbial/filtered.tinydb"
-    writer = io.write_db(outpath, create=True, if_exists="overwrite")
+    writer = io.write_db(
+        outpath, create=True, if_exists="overwrite" if overwrite else "raise"
+    )
     app = loader + just_nucs + writer
     r = app.apply_to(dstore, cleanup=True, show_progress=True, logger=LOGGER)
     click.secho("Done!", fg="green")
