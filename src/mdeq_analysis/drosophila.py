31fa96f7fd6b783c43f969a1c5be21080d792f72
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-01-27 17:18:38 +1100

    MAINT: updated dros_sample_alignments to latest cogent3 API

--- src/mdeq_analysis/drosophila.py
+++ src/mdeq_analysis/drosophila.py
@@ -2,9 +2,8 @@ import re
 
 from pathlib import Path
 
-from cogent3.app import io, sample
-from mdeq.sqlite_data_store import sql_writer
-from mdeq.utils import rich_display
+from cogent3 import get_app, open_data_store
+from mdeq.utils import rich_display, summary_not_completed, write_to_sqldb
 from rich.progress import track
 from scitrack import CachingLogger
 
@@ -56,23 +55,25 @@ def dros_sample_alignments(
     LOGGER.input_file(fb_table)
     fb_table = load_flybase_tsv(fb_table)
     fb_ids = set(fb_table.columns["primary_FBid"])
-    dstore = io.get_data_store(indir, suffix="afa.mask", limit=limit)
+    dstore = open_data_store(indir, suffix="afa.mask", limit=limit)
     if testrun:
         print(dstore)
         return True
 
-    reader = io.load_aligned(format="fasta", moltype="dna")
-    take_seqs = sample.take_named_seqs("Dmel", "Dsim", "Dyak")
+    reader = get_app("load_aligned", format="fasta", moltype="dna")
+    take_seqs = get_app("take_named_seqs", "Dmel", "Dsim", "Dyak")
     align_qual = alignment_filter()
     seq_qual = sequence_filter()
-    third_codons = sample.take_codon_positions(3)
-    no_degenerates = sample.omit_degenerates(moltype="dna", gap_is_degen=True)
-    no_shorties = sample.min_length(300)
-    writer = sql_writer(
-        outpath,
-        create=True,
-        if_exists=io.OVERWRITE if overwrite else io.RAISE,
-    )
+    third_codons = get_app("take_codon_positions", 3)
+    no_degenerates = get_app("omit_degenerates", moltype="dna", gap_is_degen=True)
+    no_shorties = get_app("min_length", 300)
+
+    if outpath.exists() and not overwrite:
+        return outpath
+
+    outpath.parent.mkdir(parents=True, exist_ok=True)
+    out_dstore = open_data_store(outpath, mode="w")
+    writer = write_to_sqldb(out_dstore)
     # we do the write as a separate step so that the source can be simplified
     app = (
         reader
@@ -84,30 +85,31 @@ def dros_sample_alignments(
         + no_shorties
     )
     success = 0
-
-    for m in track(dstore, refresh_per_second=5):
-        dmel_sym = Path(m).stem.split("_")[0]
+    for m in track(dstore.completed, refresh_per_second=5):
+        dmel_sym = m.unique_id.split("_")[0]
         if dmel_sym not in fb_ids:
             continue
 
         result = app(m)
         if not result:
-            result.source = f"{dmel_sym}.json"
+            result.source = f"{dmel_sym}"
             if verbose > 2:
                 print(result)
-            writer.data_store.write_incomplete(result.source, result)
         else:
-            result.info.source = f"{dmel_sym}.json"
+            result.info.source = f"{dmel_sym}"
             success += 1
-            writer(result)
 
-    log_file_path = LOGGER.log_file_path
+        writer(result)
+
+    log_file_path = Path(LOGGER.log_file_path)
     LOGGER.shutdown()
-    writer.data_store.add_file(log_file_path, cleanup=True, keep_suffix=True)
-    writer.data_store.close()
+    out_dstore.write_log(unique_id=log_file_path.name, data=log_file_path.read_text())
+    log_file_path.unlink()
+    out_dstore.unlock()
 
-    rich_display(writer.data_store.describe)
-    if len(writer.data_store.incomplete) > 0 and verbose:
-        rich_display(writer.data_store.summary_incomplete)
+    rich_display(out_dstore.describe)
+    if len(out_dstore.not_completed) > 0 and verbose:
+        rich_display(summary_not_completed(out_dstore))
 
+    out_dstore.close()
     return True
